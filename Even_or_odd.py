11955bdae2cf33296d7a35a58d4b0b488b314faf
METIEKA SOLARIE <metiakasolarie@gmail.com>
2025-10-29 08:49:06 +0100

    Even or Odd numbers
    This code is to verify if a number is an even or odd number in python programming. it also handles errors: that is if you input a word or a letter instead of a number, it will ask you to enter a number

---
+++ Even_or_odd.py
@@ -0,0 +1,11 @@
+try:
+
+    number = int(input("Enter a number!: "))
+
+    if number % 2 == 0:
+        print("This is an even number!")
+
+    else:
+        print("this is an odd number")
+except ValueError:
+    print("This is not a number. You must enter a number!")
